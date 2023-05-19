af690e7e2d92ab70864f26c51ad1a060c03866e1
Jake-Purton <jakecapurton@gmail.com>
2023-05-19 10:28:32 +0100

    working server & client

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy_renet::{
     renet::{
         RenetConnectionConfig, RenetError, RenetServer, ServerAuthentication,
-        ServerConfig,
+        ServerConfig, DefaultChannel,
     }, RenetServerPlugin,
 };
 
@@ -20,6 +20,11 @@ enum ServerMessages {
     PlayerPosition {id: u64, position: Vec3},
 }
 
+#[derive(Debug, Serialize, Deserialize, Component, Resource)]
+pub enum ClientMessage {
+    PlayerPosition(Vec3),
+}
+
 fn new_renet_server() -> RenetServer {
     let server_addr = "127.0.0.1:5000".parse().unwrap();
     let socket = UdpSocket::bind(server_addr).unwrap();
@@ -32,10 +37,11 @@ fn new_renet_server() -> RenetServer {
 fn main() {
 
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(MinimalPlugins);
     app.add_plugin(RenetServerPlugin::default());
     app.insert_resource(new_renet_server());
     app.add_system(panic_on_error_system);
+    app.add_system(server_update_system);
 
     app.run();
 }
@@ -45,5 +51,21 @@ fn main() {
 fn panic_on_error_system(mut renet_error: EventReader<RenetError>) {
     for e in renet_error.iter() {
         panic!("{}", e);
+    }
+}
+
+fn server_update_system(
+    mut server: ResMut<RenetServer>,
+) {
+
+    for client_id in server.clients_id().into_iter() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::Unreliable) {
+            let client_message: ClientMessage = bincode::deserialize(&message).unwrap();
+
+            match client_message {
+                ClientMessage::PlayerPosition(a) => println!("{}", a.x),
+            }
+
+        }
     }
 }
